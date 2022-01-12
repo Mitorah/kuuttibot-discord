490635049ef2bead3316677d3cfe371c3da8171a
Mitorah <leinonen.op@gmail.com>
2022-01-12 13:17:07 +0200

    Added args to commands. Now arguments can be passed after command and handled in 'execute(message, args)'

--- src/commands/who.js
+++ src/commands/who.js
@@ -5,7 +5,16 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('who')
         .setDescription('Who am I?'),
-    async execute(message) {
-        await message.reply(`Hello! My name is KuuttiBot!`)
+    async execute(message, args) {
+        var argComment = ""
+        
+        args.forEach(argument => {
+            argComment += argument + " "
+        });
+        
+        if (args.length > 0)
+            await message.reply(`Hello! My name is KuuttiBot! I got ${args.length} arguments: ${argComment}`)
+        else
+            await message.reply(`Hello! My name is KuuttiBot!`)
     }
 }
